9a2bc1b50fffb85416b8d9978abb98205fb4ee1b
Ivo Lopes <charro.il@gmail.com>
2018-08-28 19:52:21 +0200

    add login activity

---
+++ app/src/main/java/com/charroapps/smackchat/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.charroapps.smackchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateUserClicked(view : View){
+
+    }
+
+    fun loginLoginUserClicked(view: View){
+
+    }
+}
