d56300d7d48587944a06e0abc03eb2ba5baef360
Ivo Lopes <charro.il@gmail.com>
2018-08-31 15:32:30 +0200

    added broadcast receiver

--- app/src/main/java/com/charroapps/smackchat/Controller/MainActivity.kt
+++ app/src/main/java/com/charroapps/smackchat/Controller/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.charroapps.smackchat.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.charroapps.smackchat.R
+import com.charroapps.smackchat.Services.AuthService
+import com.charroapps.smackchat.Utilities.BROADCAST_USER_DATA_CHANGE
+import com.charroapps.smackchat.Utilities.UserDataService
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +30,25 @@ class MainActivity : AppCompatActivity() {
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,
+                IntentFilter(BROADCAST_USER_DATA_CHANGE))
+
+    }
+
+    private val userDataChangeReceiver = object: BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            //When Broadcast is send out
+            if(AuthService.isLoggenIn){
+                //The user is logged in
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                loginBtnNavHeader.text = "Logout"
+            }
+        }
 
     }
 
@@ -38,8 +66,18 @@ class MainActivity : AppCompatActivity() {
 
     fun loginBtnNavClicked(view: View){
 
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+        if(AuthService.isLoggenIn){
+            //logout
+            UserDataService.logout()
+            userNameNavHeader.text = ""
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text = "Login"
+        }else{
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
     fun sendMessageBtnClicked(view: View){
